9a76a0e7de8fed7f535a74fcf5e3b2d2565e6d9e
Roonny Ruiz <roonny7@gmail.com>
2026-04-07 22:29:08 -0500

    cambios iniciales

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Contenido } from './components/Contenido'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { SideNav } from './components/SideNav'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <SideNav />
+      <Contenido />
+      <Footer />
+    </>
+  )
+}
+
+export default App
