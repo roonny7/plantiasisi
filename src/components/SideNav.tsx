85198a91c1003a51bc4bedaa402b87d9ffede4ce
Roonny Ruiz <roonny7@hotmail.com>
2026-04-10 09:51:25 -0500

    sidebar

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -3,34 +3,15 @@ export const SideNav = () => {
   return (
     <>
             {/* Main Sidebar Container */}
-            <aside className="main-sidebar sidebar-dark-primary elevation-4">
-            {/* Brand Logo */}
-            <a href="index3.html" className="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{opacity: '.8'}} />
-                <span className="brand-text font-weight-light">Gilucho power</span>
-            </a>
+            <aside className="main-sidebar sidebar-dark-primary elevation-4">            
+                <img src="Escudo.png" alt="Indicadores" className="img-responsive" style={{opacity: '.8', width : '70px', marginLeft: 'auto', marginRight: 'auto'}} />                 
             {/* Sidebar */}
             <div className="sidebar">
                 {/* Sidebar user panel (optional) */}
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-                <div className="image">
-                    <img src="dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+                
                 </div>
-                <div className="info">
-                    <a href="#" className="d-block">Recórcholis</a>
-                </div>
-                </div>
-                {/* SidebarSearch Form */}
-                <div className="form-inline">
-                <div className="input-group" data-widget="sidebar-search">
-                    <input className="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search" />
-                    <div className="input-group-append">
-                    <button className="btn btn-sidebar">
-                        <i className="fas fa-search fa-fw" />
-                    </button>
-                    </div>
-                </div>
-                </div>
+
                 {/* Sidebar Menu */}
                 <nav className="mt-2">
                 <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
